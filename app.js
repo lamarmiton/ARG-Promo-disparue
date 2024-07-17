102217165474f5b85bfbdf4af47484f99e103c9c
Jérémy Richard <lama@air-de-jeremy.home>
2024-07-17 15:48:43 +0200

    Add sqlite database for users accounts. Users now can only enter the good answers at the first question. Login page only accessible after the first good answer

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const session = require('express-session');
+
+function checkAuth(req, res, next) {
+  if (req.session.userId) {
+    next();
+  } else {
+    res.status(401).send("Non autorisé");
+  }
+}
+
 const app = express();
 const port = 3000;
+
+const db = new sqlite3.Database('./dblite.db', (err) => {
+  if (err) {
+    console.error('Erreur lors de l\'ouverture de la base de données', err.message);
+  } else {
+    console.log('Connecté à la base de données SQLite.');
+  }
+});
+
+app.use(session({
+  secret: 'EC779F39-F57E-4AB9-A8A4-E8027F367418',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Mettez à true si vous êtes derrière un proxy HTTPS
+}));
+
+db.serialize(() => {
+  // Créer une nouvelle table
+  db.run("CREATE TABLE IF NOT EXISTS account (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT NOT NULL, password TEXT NOT NULL)");
+  // Insérer des données
+  db.run("INSERT INTO account (user, password) VALUES ('admin', 'F9A68BCF-E0F4-40C1-A4CD-6CF0009483AD')");
+});
 
 app.use(express.json()); // Pour parser les requêtes JSON
 app.use(express.static('public')); // Ajout pour servir les fichiers statiques
@@ -9,6 +42,33 @@ app.get('/', (req, res) => {
   res.send('La promotion disparue');
 });
 
+app.post('/login', (req, res) => {
+  const { user, password } = req.body;
+  db.get("SELECT * FROM account WHERE user = ?", [user], (err, row) => {
+    if (err) {
+      res.status(500).send("Erreur lors de la recherche de l'utilisateur");
+    } else if (row && password == row.password) {
+      req.session.userId = row.id; // Stocker l'ID utilisateur dans la session
+      res.send("Connexion réussie");
+    } else {
+      res.send("Nom d'utilisateur ou mot de passe incorrect");
+    }
+  });
+});
+
+app.get('/page-secrete', checkAuth, (req, res) => {
+  res.send("Vous êtes connecté et pouvez voir cette page.");
+});
+
 app.listen(port, () => {
   console.log(`Serveur démarré sur http://localhost:${port}`);
+});
+
+// Fermer la base de données
+db.close((err) => {
+  if (err) {
+    console.error('Erreur lors de la fermeture de la base de données', err.message);
+  } else {
+    console.log('Connexion à la base de données fermée.');
+  }
 });
--- public/js/dechiffre.js
+++ public/js/dechiffre.js
@@ -1,3 +1,32 @@
+function toggleMenu() {
+    var navLinks = document.getElementById("navLinks");
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "block";
+    }
+}
+document.addEventListener('DOMContentLoaded', function() {
+    // Vérifie si la réponse a déjà été donnée correctement
+    if (localStorage.getItem('enigmeResolue') === 'true') {
+        let elementsChiffres = document.querySelectorAll('.chiffre');
+        elementsChiffres.forEach(function(element) {
+            let contenuChiffre = element.innerText;
+            let contenuDechiffre = dechiffreTexte(contenuChiffre, 13);
+            element.innerText = contenuDechiffre;
+        });
+        var formulaire = document.getElementById('formulaire-dechiffrement');
+        formulaire.parentNode.removeChild(formulaire);
+        var navLinks = document.getElementById('navLinks');
+        var listItem = document.createElement('li');
+        var link = document.createElement('a');
+        link.href = 'login.html';
+        link.textContent = 'Connexion'; 
+        listItem.appendChild(link);
+        navLinks.appendChild(listItem);
+    }
+});
+
 function dechiffreTexte(texteChiffre, decalage) {
     return texteChiffre.split('').map(char => {
         let code = char.charCodeAt(0);
@@ -13,12 +42,42 @@ function dechiffreTexte(texteChiffre, decalage) {
 function demanderCleEtDechiffrer() {
     let cle = parseInt(document.getElementById('cle-dechiffrement').value, 10);
     if (!isNaN(cle)) {
-        let elementsChiffres = document.querySelectorAll('.chiffre');
-        elementsChiffres.forEach(function(element) {
-            let contenuChiffre = element.innerText;
-            let contenuDechiffre = dechiffreTexte(contenuChiffre, cle);
-            element.innerText = contenuDechiffre;
-        });
+        if (cle == 13) {
+            var video = document.getElementById('vhsVideo');
+            video.style.display = 'block';
+            video.play();
+        
+            video.onended = function() {
+                video.style.display = 'none';
+            };
+
+            let elementsChiffres = document.querySelectorAll('.chiffre');
+            elementsChiffres.forEach(function(element) {
+                let contenuChiffre = element.innerText;
+                let contenuDechiffre = dechiffreTexte(contenuChiffre, cle);
+                element.innerText = contenuDechiffre;
+            });
+
+            localStorage.setItem('enigmeResolue', 'true');
+            var formulaire = document.getElementById('formulaire-dechiffrement');
+            formulaire.parentNode.removeChild(formulaire);
+            localStorage.setItem('enigmeResolue', 'true');
+            if (localStorage.getItem('enigmeResolue') === 'true') {
+                var navLinks = document.getElementById('navLinks');
+                var listItem = document.createElement('li');
+                var link = document.createElement('a');
+                link.href = 'login.html';
+                link.textContent = 'Connexion'; 
+                listItem.appendChild(link);
+                navLinks.appendChild(listItem);
+            }
+        }
+        else {
+            document.body.classList.add('errorAnimation');
+            setTimeout(function() {
+                document.body.classList.remove('errorAnimation');
+            }, 250);
+        }
     } else {
         alert("Clé invalide. Veuillez entrer un nombre.");
     }
@@ -26,14 +85,6 @@ function demanderCleEtDechiffrer() {
 
 document.getElementById('formulaire-dechiffrement').addEventListener('submit', function(e) {
     e.preventDefault(); // Empêche la soumission réelle du formulaire
-
-    var video = document.getElementById('vhsVideo');
-    video.style.display = 'block'; // Affiche la vidéo
-    video.play(); // Commence à jouer la vidéo
-
-    video.onended = function() {
-        video.style.display = 'none'; // Cache la vidéo une fois qu'elle est terminée
-    };
 });
 
 document.addEventListener('DOMContentLoaded', function() {
